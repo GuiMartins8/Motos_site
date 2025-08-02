9913a9a790cdde4e8a2e3edf4ccba0be04e310cc
Ittllo <itajaruzo@gmail.com>
2025-08-02 15:23:47 -0300

    criado alguns script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function mostrarMotos() {
+  document.getElementById('moto-lista').style.display = 'flex';
+}
+
+const motos = [
+  { nome: 'moto1', img: 'assets/images/moto1', link: 'motos/moto1.html' },
+  { nome: 'moto2', img: 'assets/images/moto2', link: 'motos/moto2.html' },
+  { nome: 'moto3', img: 'assets/images/moto3', link: 'motos/moto3.html' },
+  { nome: 'moto4', img: 'assets/images/moto4', link: 'motos/moto4.html' },
+  { nome: 'moto5', img: 'assets/images/moto5', link: 'motos/moto5.html' },
+];
